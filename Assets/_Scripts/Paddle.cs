cb75ab906064401377d70545e716185c8c1893c4
McMorn <feliksik.pawel@gmail.com>
2015-11-09 12:07:55 +0100

    Initial commit

---
+++ Assets/_Scripts/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+    public float paddleSpeed = 1;
+    Vector3 playerPos;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        float yPos = gameObject.transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
+        playerPos = new Vector3(-20, Mathf.Clamp(yPos, -12.5f, 12.5f));
+        gameObject.transform.position = playerPos;
+	}
+}
---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    int randX;
+    int randY;
+
+    public GameObject ball;
+
+	void Start ()
+    {
+        Spawn();
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Spawn();
+        }
+	}
+
+    void CreateRandomPosition()
+    {
+        randX = Random.Range(-8, 8);
+        randY = Random.Range(-4, 4);
+    }
+
+    void Spawn()
+    {
+        CreateRandomPosition();
+
+        Instantiate(ball, new Vector3(randX, randY), Quaternion.identity);
+    }
+}
